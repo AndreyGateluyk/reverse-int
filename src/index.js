2137b9e0d25383dccf2e02acfcc0ca9ce0b4d80c
Andrey Gateluyk <gatelyuk97@mail.ru>
2023-10-23 22:48:45 +0300

    complite task#2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 module.exports = function reverse (n) {
-  let result = n.toString().split('').reverse().join('')
+  let arr = n.toString().split('')
+  if(arr[0] = '-') {
+    arr.splice(0,1)
+  }
+  if(arr[arr.length-1] === '0') {
+    arr.splice(arr.length-1, 1)
+  }
+  let result = arr.reverse().join('')
   return result
 }
